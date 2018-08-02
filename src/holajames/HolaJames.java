d26387adb516722b65f3dc071cea00bb7b52abb0
JamesGonzalez <jamesanthony-gonzalezgramajo@cunc.edu.gt>
2018-08-01 21:07:34 -0600

    acabo de colocar el segundo cambio al codigo

--- src/holajames/HolaJames.java
+++ src/holajames/HolaJames.java
@@ -8,6 +8,8 @@ public class HolaJames {
 
     public static void main(String[] args) {
         System.out.println("HOLA JAMES");
+        
+        System.out.println("Este es el primer cambio en el codigo");
     }
     
 }
